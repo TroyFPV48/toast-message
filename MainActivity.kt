9d104c49308d112ad6e64f877de7f314851cc752
Tuong Pham Minh <77989401+TroyFPV48@users.noreply.github.com>
2023-04-30 18:42:40 -0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.toastmessage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myButton = findViewById<Button>(R.id.button)
+        myButton.setOnClickListener {
+            Toast.makeText(this, "Stop touching me!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
